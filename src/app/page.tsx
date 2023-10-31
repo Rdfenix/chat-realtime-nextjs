a3bd62c152192e7f43dca1a2b1809b7a54c67d5c
rdfenix <rhoodsk8erboy@gmail.com>
2023-10-30 23:14:16 -0300

    create: adding redirect method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import styles from "./page.module.scss";
 import Image from "next/image";
 import logo from "@/app/shared/icons/Let_s_Talk.png";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : "";
+
+  useEffect(() => {
+    if (token) {
+      router.push("/main");
+    }
+  }, [router, token]);
+
   return (
     <section className={styles.home_section}>
       <Image src={logo} priority alt="logo" />
--- src/app/shared/layout/login-layout/index.tsx
+++ src/app/shared/layout/login-layout/index.tsx
@@ -1,12 +1,23 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import styles from "./login-layout.module.scss";
+import { useRouter } from "next/navigation";
 
 interface SignInSignOutLayoutProps {
   children: ReactNode;
 }
 
 function SignInSignOutLayout({ children }: SignInSignOutLayoutProps) {
+  const router = useRouter();
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : "";
+
+  useEffect(() => {
+    if (token) {
+      router.push("/main");
+    }
+  }, [router, token]);
+
   return <section id={styles.signin_signout_section}>{children}</section>;
 }
 
